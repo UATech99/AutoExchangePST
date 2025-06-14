f06660d1c7972001c5916cf3f154e0d3676a07a4
Umar Adil <umaradil99@gmail.com>
2025-06-15 01:33:33 +0500

    Some General Functions Added

--- Send-Notification.ps1
+++ Send-Notification.ps1
@@ -4,9 +4,9 @@ param (
 )
 
 # Email settings
-$From = "EDLAutomation@edl2.com"
-$To = "umar@edl2.com"
-$SmtpServer = "Exchangeedl2.edl2.com"
+$From = "<Your-Email-From-System-Has-TO-Send-Emails>" # AutomatedPST@example.com
+$To = "<To-Whom-Emails-Should-Received>" #UserName@example.com
+$SmtpServer = "<SMTP-Server-FQDN>" # smtp.example.com
 #$SmtpPort = 25
 
 Send-MailMessage -From $From -To $To -Subject $Subject -Body $Body -SmtpServer $SmtpServer
